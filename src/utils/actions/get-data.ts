5f4509427df4a2b07685724c45c26fbc60f38b83
Matheus <matheus_lopes151@hotmail.com>
2025-01-19 12:40:26 -0300

    corrigindo api

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,6 +1,7 @@
 export async function getDataHome() {
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}//objects/678bfb1cc41bec43e4330751?pretty=true&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,type`)
+
         if (!res.ok) {
             throw new Error("Failed to fetch dataA")
         }
